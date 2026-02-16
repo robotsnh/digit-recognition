a8635b9068fd61a52ab0c9e4206c20765293dd66
gamer10297 <159864804+gamer10297@users.noreply.github.com>
2026-02-16 19:05:01 +0000

    Created `forward` function for class `Neuron`

--- src/neuron.py
+++ src/neuron.py
@@ -3,8 +3,4 @@ class Neuron:
         self.weight = 0
         self.bias = 0
         self.last_input = None
-    def activation_function(self, x):
-        return 1 / (1 + math.exp(-x))
-    def forward(self, input):
-        self.last_input = input
-        return self.activation_function(self.weight * input + self.bias)
+    
