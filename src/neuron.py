8d096969d13b6fabbdaa9def53c99cbf9414797e
gamer10297 <159864804+gamer10297@users.noreply.github.com>
2026-02-16 20:53:31 +0000

    Added back activation function and forward function

--- src/neuron.py
+++ src/neuron.py
@@ -5,8 +5,16 @@ class Neuron:
         self.weight = 0
         self.bias = 0
         self.last_input = None
+<<<<<<< HEAD
     def _sigmoid(self, x):
         return 1 / (1 + math.exp(-x))
     def forward(self, input):
         self.last_input = input
         return self._sigmoid(self.weight * input + self.bias)
+=======
+    def activation_function(self, x):
+        return 1 / (1 + math.exp(-x))
+    def forward(self, input):
+        self.last_input = input
+        return self.activation_function(self.weight * input + self.bias)
+>>>>>>> d54d40a (Added back activation function and forward function)
