d7e9241cc169ffa2ff765f5f5cba43ae12ac5f95
robotsnh <88983536+robotsnh@users.noreply.github.com>
2026-02-17 08:52:47 -0800

    Added a digit argument to `Image.__init__` and refactored accordingly

--- src/image.py
+++ src/image.py
@@ -1,4 +1,10 @@
+class MisstructuredImageException(Exception):
+    ...
+
 class Image(object):
-    def __init__(self, data: list[int]):
+    def __init__(self, digit: int, data: list[list[int | float]]):
+        for d in data:
+            if not isinstance(d, list):
+                raise MisstructuredImageException("The digit argument should be a list of lists, where each sub-list is a row.")
         self.data = data
-        self.digit = data[0]
+        self.digit = digit
