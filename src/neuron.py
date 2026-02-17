3a20c41aa855e910d3963ce40452f7056277b8a6
gamer10297 <159864804+gamer10297@users.noreply.github.com>
2026-02-17 17:42:23 +0000

    Fixed `forward` function, now has multiple inputs and outputs

--- src/neuron.py
+++ src/neuron.py
@@ -2,10 +2,14 @@ class Neuron:
     def __init__(self):
         self.weight = 0
         self.bias = 0
-        self.last_input = None
+        self.last_inputs = None
     def activation_function(self, x):
         return 1 / (1 + math.exp(-x))
-    def forward(self, input):
-        self.last_input = input
-        return self.activation_function(self.weight * input + self.bias)
-    
+    def forward(self, inputs):
+        self.last_inputs = inputs
+        self.outputs = []
+        for i in inputs:
+            i*=self.weight
+            i+=self.bias
+            self.outputs.append(activation_function(i))
+        return self.outputs
