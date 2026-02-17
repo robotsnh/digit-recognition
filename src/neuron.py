18c2f4213ea18767e72676b76980b4b9576b7555
robotsnh <88983536+robotsnh@users.noreply.github.com>
2026-02-17 07:16:26 -0800

    Transferred the `sigmoid` function to `src/mathHelper.py`

--- src/neuron.py
+++ src/neuron.py
@@ -1,12 +1,11 @@
 import math
+from mathHelper import sigmoid
 
 class Neuron:
     def __init__(self):
         self.weight = 0
         self.bias = 0
         self.last_input = None
-    def activation_function(self, x):
-        return 1 / (1 + math.exp(-x))
     def forward(self, input):
         self.last_input = input
-        return self.activation_function(self.weight * input + self.bias)
+        return sigmoid(self.weight * input + self.bias)
