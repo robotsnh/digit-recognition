f6516f2de210164c076a74918246565089495643
robotsnh <88983536+robotsnh@users.noreply.github.com>
2026-02-17 07:37:53 -0800

    Create more of a `CSVFile` class

--- src/csvHelper.py
+++ src/csvHelper.py
@@ -1,8 +1,18 @@
-import os.path
+import os.path, csv
+
+class NotACSVFileException(Exception):
+    ...
 
 class CSVFile(object):
-    __slots__ = ["path"]
+    __slots__ = ["path", "rows"]
     def __init__(self, path: str):
         if not os.path.isfile(path):
             raise FileNotFoundError
+        if not path[-4:] == ".csv":
+            raise NotACSVFileException
         self.path = path
+        self.rows = []
+        with open(self.path) as file:
+            reader = csv.reader(file)
+            for row in reader:
+                self.rows.append(row)
