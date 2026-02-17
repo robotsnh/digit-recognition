db1e42719f97bb8df70ed279024e0d6410fecf7e
robotsnh <88983536+robotsnh@users.noreply.github.com>
2026-02-17 08:23:17 -0800

    Added __greatly__ to the `Dataset` class

--- src/dataset.py
+++ src/dataset.py
@@ -1,3 +1,11 @@
+from csvHelper import CSVFile
+from image import Image
+
 class Dataset(object):
-    def __init__(self, dataFile):
-        ...
+    __slots__ = ["rows", "images"]
+    def __init__(self, dataFile: CSVFile):
+       self.rows = dataFile.rows
+       self.images = []
+
+       for image in self.rows:
+           self.images.append(Image(image))
