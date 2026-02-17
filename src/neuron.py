0dca4e943f5b56fbf42583b48a0b1f590270d130
robotsnh <88983536+robotsnh@users.noreply.github.com>
2026-02-17 09:41:56 -0800

    Changes requested by @gamer10297 completed

--- src/neuron.py
+++ src/neuron.py
@@ -1,11 +1,12 @@
 import math
-from mathHelper import sigmoid
 
 class Neuron:
     def __init__(self):
         self.weight = 0
         self.bias = 0
         self.last_input = None
+    def _sigmoid(self, x):
+        return 1 / (1 + math.exp(-x))
     def forward(self, input):
         self.last_input = input
-        return sigmoid(self.weight * input + self.bias)
+        return self._sigmoid(self.weight * input + self.bias)
